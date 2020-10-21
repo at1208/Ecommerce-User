f01249605811d466a153ce060338c45e819b919b
at1208 <amantiwari12081999@gmail.com>
2020-10-21 13:05:33 +0530

    bugs fixed

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,14 @@ import openSocket from 'socket.io-client';
 
 
 const App = ({ data }) => {
+  console.log(data)
 useEffect(() => {
   const socket = openSocket(process.env.NEXT_PUBLIC_SERVER);
   if(process.browser===window){socket.on('online')}
 },[])
-
+// !(data.result == null)
  const showCarousel = () => {
-   return !(data.result == null) && data.map((item,i) => {
+   return  data && data.map((item,i) => {
      return <Fragment key={i}>
               <CarouselContainer products={item} />
             </Fragment>
